5891b1024e9c970d18a04569a55b31e273420c55
quriqyl <krysanov-k@mail.ru>
2026-09-06 08:36:00 +0300

    1.6

--- src/chapter_1/Task_6.java
+++ src/chapter_1/Task_6.java
@@ -1,4 +1,15 @@
 package chapter_1;
 
+import java.util.Scanner;
+
 public class Task_6 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = a*10 + b;
+        System.out.println(c);
+
+        scanner.close();
+    }
 }
