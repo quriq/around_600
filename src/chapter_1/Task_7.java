ac3a8c2d160ef3c826d4ed214353b629c6b2eb28
quriqyl <krysanov-k@mail.ru>
2026-09-06 09:05:41 +0300

    1.7

--- src/chapter_1/Task_7.java
+++ src/chapter_1/Task_7.java
@@ -1,4 +1,12 @@
 package chapter_1;
 
+import java.util.Scanner;
+
 public class Task_7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        double y = x/3;
+        System.out.println("x=" + x + " y=" + y);
+    }
 }
