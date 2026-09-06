4fe9466bbd4df4f744073f1348e09581d80a1d17
quriqyl <krysanov-k@mail.ru>
2026-09-06 08:18:23 +0300

    1.5

--- src/chapter_1/Task_5.java
+++ src/chapter_1/Task_5.java
@@ -1,4 +1,10 @@
 package chapter_1;
 
 public class Task_5 {
+    public static void main(String[] args){
+        double x = 7.5;
+        double a = x*2;
+        double b = x*3;
+        System.out.println("x=" + x +" a=" + a + " b="+ b);
+    }
 }
