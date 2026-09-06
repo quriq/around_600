0efc55e81beb2fc1da1e1a672f20ad39669acbfc
quriqyl <krysanov-k@mail.ru>
2026-09-06 08:05:35 +0300

    1.4

--- src/chapter_1/Task_4.java
+++ src/chapter_1/Task_4.java
@@ -1,4 +1,10 @@
 package chapter_1;
 
 public class Task_4 {
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 9;
+        int third = first + second;
+        System.out.println(third);
+    }
 }
